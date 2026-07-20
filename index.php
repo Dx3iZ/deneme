4fcd4362ba12bf907b7622e8c78e003cd8ef4d73
DxeiZ <dxeiz@proton.me>
2026-07-20 05:13:19 +0400

    Add index.php with basic HTML structure

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="referrer" content="no-referrer">
+   
+    <title>Hacked By TurkHackTeam | Saldırı Timleri</title>
+    <link rel="icon" type="image/png" href="https://i.hizliresim.com/9x1hxd2.png">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body, html {
+            height: 100%;
+            background-color: #050505;
+            color: #ffffff;
+            font-family: 'Rajdhani', sans-serif;
+            overflow: hidden;
+            user-select: none;
+            cursor: crosshair;
+        }
+
+        .bg-layer {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-position: center;
+            z-index: 1;
+            opacity: 1;
+            transform: scale(1.1);
+        }
+    </style>
+</head>
+<body>
+    <div class="bg-layer">
+        <a href="https://www.turkhackteam.org/" target="_blank" title="TurkHackTeam Forumuna Git">
+            <img src="https://i.hizliresim.com/cumn9w6.gif" alt="TurkHackTeam Logo 1" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); width: 80%; height: auto; z-index: 2;">
+        </a>
+    </div>
+</body>
+</html>
